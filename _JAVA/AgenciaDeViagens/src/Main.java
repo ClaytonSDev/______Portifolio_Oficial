b744a7e64c3b60806cd1ac373ac589181138f71e
projetos <claytonsalesdev@gmail.com>
2024-11-24 16:04:23 -0300

    .
    .

--- _JAVA/AgenciaDeViagens/src/Main.java
+++ _JAVA/AgenciaDeViagens/src/Main.java
@@ -7,16 +7,26 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         PassagemController controller = new PassagemController();
 
+        String[] destinos = {"Rio de Janeiro", "Arraial do Cabo-RJ", "Belo Horizonte-MG", "Goiania-GO"};
+
         while (true) {
             System.out.print("Digite o seu nome: ");
             String nome = scanner.nextLine();
             System.out.print("Digite o seu CPF: ");
             String cpf = scanner.nextLine();
 
-            System.out.print("Digite a origem: ");
+            // Solicitar origem ao passageiro
+            System.out.print("Digite a cidade de origem: ");
             String origem = scanner.nextLine();
-            System.out.print("Digite o destino: ");
-            String destino = scanner.nextLine();
+
+            // Mostrar opções de destinos
+            System.out.println("Escolha um destino:");
+            for (int i = 0; i < destinos.length; i++) {
+                System.out.println((i + 1) + ". " + destinos[i]);
+            }
+            System.out.print("Digite o número do destino escolhido: ");
+            int escolhaDestino = scanner.nextInt();
+            String destino = destinos[escolhaDestino - 1];
 
             int numeroAssento;
             LocalDate dataIda;
@@ -30,6 +40,7 @@ public class Main {
                 dataIda = LocalDate.parse(scanner.next());
                 System.out.print("Digite o horário de ida (HH:MM): ");
                 horarioIda = LocalTime.parse(scanner.next());
+                System.out.println("Obrigado por nos escolher, tenha uma otima viagem ");
 
                 assentoDisponivel = controller.verificarDisponibilidadeAssento(numeroAssento, dataIda, horarioIda);
 
@@ -51,6 +62,7 @@ public class Main {
             controller.listarPassagens();
             controller.mostrarAssentosDisponiveis();
 
+            System.out.println("Obrigado por nos escolher, tenha uma otima viagem ");
             System.out.println("Valor da Passagem: R$ " + passagem.getValor());
             System.out.println("Total a Pagar: R$ " + controller.getTotalVendas());
 
@@ -65,4 +77,5 @@ public class Main {
         scanner.close();
     }
 }
+
 
